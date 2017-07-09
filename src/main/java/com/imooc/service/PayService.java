5ca1472f3c011e34e36e4cbe894dec584035a068
廖师兄 <null@163.com>
2017-07-09 10:43:07 +0800

    发起支付, 后端代码完成

---
+++ src/main/java/com/imooc/service/PayService.java
@@ -0,0 +1,13 @@
+package com.imooc.service;
+
+import com.imooc.dto.OrderDTO;
+
+/**
+ * 支付
+ * Created by 廖师兄
+ * 2017-07-04 00:53
+ */
+public interface PayService {
+
+    void create(OrderDTO orderDTO);
+}
