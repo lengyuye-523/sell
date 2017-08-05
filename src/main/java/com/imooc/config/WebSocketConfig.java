09c1e43908fa207aeb7b52ad7dcfd3a5e504fc33
廖师兄 <null@163.com>
2017-08-05 23:04:07 +0800

    mybatis注解的使用

--- src/main/java/com/imooc/config/WebSocketConfig.java
+++ src/main/java/com/imooc/config/WebSocketConfig.java
@@ -1,14 +1,13 @@
 package com.imooc.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
  * Created by 廖师兄
  * 2017-07-30 23:17
  */
-@Component
+//@Component
 public class WebSocketConfig {
 
     @Bean
