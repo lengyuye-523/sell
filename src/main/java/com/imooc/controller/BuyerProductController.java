e7d406f341841681f5752f4099f0a1bacea8b525
廖师兄 <null@163.com>
2019-01-31 16:10:21 +0800

    微信授权(补充)

--- src/main/java/com/imooc/controller/BuyerProductController.java
+++ src/main/java/com/imooc/controller/BuyerProductController.java
@@ -10,7 +10,6 @@ import com.imooc.service.ProductService;
 import com.imooc.utils.ResultVOUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,8 +35,8 @@ public class BuyerProductController {
     private CategoryService categoryService;
 
     @GetMapping("/list")
-    @Cacheable(cacheNames = "product", key = "#sellerId", condition = "#sellerId.length() > 3", unless = "#result.getCode() != 0")
-    public ResultVO list(@RequestParam("sellerId") String sellerId) {
+//    @Cacheable(cacheNames = "product", key = "#sellerId", condition = "#sellerId.length() > 3", unless = "#result.getCode() != 0")
+    public ResultVO list(@RequestParam(value = "sellerId", required = false) String sellerId) {
         //1. 查询所有的上架商品
         List<ProductInfo> productInfoList = productService.findUpAll();
 
