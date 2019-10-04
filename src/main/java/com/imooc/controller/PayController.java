6fddd060cf390e075c9e7dd61a8b45b45fb3a3d2
廖师兄 <null@163.com>
2019-10-04 23:40:42 +0800

    支付完成后跳转的地址，编码处理

--- src/main/java/com/imooc/controller/PayController.java
+++ src/main/java/com/imooc/controller/PayController.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Map;
 
 /**
@@ -31,7 +33,7 @@ public class PayController {
     @GetMapping("/create")
     public ModelAndView create(@RequestParam("orderId") String orderId,
                                @RequestParam("returnUrl") String returnUrl,
-                               Map<String, Object> map) {
+                               Map<String, Object> map) throws UnsupportedEncodingException {
         //1. 查询订单
         OrderDTO orderDTO = orderService.findOne(orderId);
         if (orderDTO == null) {
@@ -42,7 +44,7 @@ public class PayController {
         PayResponse payResponse = payService.create(orderDTO);
 
         map.put("payResponse", payResponse);
-        map.put("returnUrl", returnUrl);
+        map.put("returnUrl", returnUrl.startsWith("http://") ? returnUrl : URLEncoder.encode(returnUrl, "utf-8"));
 
         return new ModelAndView("pay/create", map);
     }
